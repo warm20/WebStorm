af859946ff162bbd6f9dfe5e1ee235b4b262327b
warm20 <du_lingli@163.com>
2023-12-17 20:41:10 +0800

    upload to github

--- demo.cpp
+++ demo.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace Storm;
 
 int main(){
-    auto req = MakeHttpReq("GET /A/// HTTP/1.1");
+    auto req = MakeHttpReq("GET /A/?a=10&b=10 HTTP/1.1\nHost: www.hello.com\nRenferer: cn.bing.com\n");
     auto v = UrlParse(req.req_url);
     for(auto i : v) {
         cout << i << endl;
